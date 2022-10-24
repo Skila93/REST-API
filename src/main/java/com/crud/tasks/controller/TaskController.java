33f7764b272e85e9cffd47b8f8cd43212c4d8adf
Konrad <skalskikonrad@gmail.com>
2022-10-24 15:18:38 +0200

    Zad. 18.4 dodane

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -10,22 +10,22 @@ import java.util.List;
 @RequestMapping("v1/tasks")
 public class TaskController {
 //    @RequestMapping(method = RequestMethod.GET, value = "")
-    @GetMapping(value = "getTasks")
+    @GetMapping
     public List<TaskDto> getTasks(){
         return new ArrayList<>();
     }
-    @GetMapping(value = "getTask")
+    @GetMapping(value = "{taskId}")
     public TaskDto getTask(Long taskId){
         return new TaskDto(1L,"test title", "test_content");
     }
-    @DeleteMapping(value = "deleteTask")
+    @DeleteMapping
     public void deleteTask(Long taskId){
     }
-    @PutMapping(value = "updateTask")
+    @PutMapping
     public TaskDto updateTask(TaskDto task){
         return new TaskDto(1L, "Edited test title", "Test content");
     }
-    @PostMapping(value = "createTask")
+    @PostMapping
     public void createTask(TaskDto taskDto){
     }
 }
