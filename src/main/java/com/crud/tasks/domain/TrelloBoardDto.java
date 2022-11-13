9e376bd6da83bee17f27ebc3b5950702b9106597
Konrad <skalskikonrad@gmail.com>
2022-11-13 14:49:13 +0100

    Excercise 22.3 added

--- src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
@@ -1,15 +1,27 @@
 package com.crud.tasks.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 /*@Getter
 @AllArgsConstructor
 @NoArgsConstructor*/
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TrelloBoardDto {
+
+    @JsonProperty("id")
     private String name;
+
+    @JsonProperty("name")
     private String id;
+
+    @JsonProperty("lists")
+    private List<TrelloListDto> lists;
 }
